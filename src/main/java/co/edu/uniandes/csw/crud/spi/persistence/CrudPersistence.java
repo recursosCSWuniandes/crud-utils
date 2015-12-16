1895cb4be60e0bfc51fc89e200885f4ad3789721
Andrés Felipe Esguerra Restrepo <kaosterra@gmail.com>
2015-12-16 16:42:26 -0500

    Corrección CurdPersistence

--- src/main/java/co/edu/uniandes/csw/crud/spi/persistence/CrudPersistence.java
+++ src/main/java/co/edu/uniandes/csw/crud/spi/persistence/CrudPersistence.java
@@ -7,40 +7,40 @@ import javax.persistence.Query;
 
 public abstract class CrudPersistence<T> {
 
-    protected EntityManager em;
+    protected abstract EntityManager getEntityManager();
 
-    protected Class<T> entityClass;
+    protected abstract Class<T> getEntityClass();
 
     public int count() {
-        Query count = em.createQuery("select count(u) from " + entityClass.getSimpleName() + " u");
+        Query count = getEntityManager().createQuery("select count(u) from " + getEntityClass().getSimpleName() + " u");
         return Integer.parseInt(count.getSingleResult().toString());
     }
 
     public T create(T entity) {
-        em.persist(entity);
+        getEntityManager().persist(entity);
         return entity;
     }
 
     public T update(T entity) {
-        return em.merge(entity);
+        return getEntityManager().merge(entity);
     }
 
     public void delete(Long id) {
-        T entity = em.find(entityClass, id);
-        em.remove(entity);
+        T entity = getEntityManager().find(getEntityClass(), id);
+        getEntityManager().remove(entity);
     }
 
     public T find(Long id) {
-        return em.find(entityClass, id);
+        return getEntityManager().find(getEntityClass(), id);
     }
 
     public List<T> findAll() {
-        Query q = em.createQuery("select u from " + entityClass.getSimpleName() + " u");
+        Query q = getEntityManager().createQuery("select u from " + getEntityClass().getSimpleName() + " u");
         return q.getResultList();
     }
 
     public List<T> findAll(Integer page, Integer maxRecords) {
-        Query q = em.createQuery("select u from " + entityClass.getSimpleName() + " u");
+        Query q = getEntityManager().createQuery("select u from " + getEntityClass().getSimpleName() + " u");
         if (page != null && maxRecords != null) {
             q.setFirstResult((page - 1) * maxRecords);
             q.setMaxResults(maxRecords);
@@ -49,11 +49,11 @@ public abstract class CrudPersistence<T> {
     }
 
     public <V> List<V> executeListNamedQuery(String name) {
-        return em.createNamedQuery(name).getResultList();
+        return getEntityManager().createNamedQuery(name).getResultList();
     }
 
     public <V> List<V> executeListNamedQuery(String name, Map<String, Object> params) {
-        Query q = em.createNamedQuery(name);
+        Query q = getEntityManager().createNamedQuery(name);
         for (Map.Entry<String, Object> entry : params.entrySet()) {
             q.setParameter(entry.getKey(), entry.getValue());
         }
@@ -61,11 +61,11 @@ public abstract class CrudPersistence<T> {
     }
 
     public <V> V executeSingleNamedQuery(String name) {
-        return (V) em.createNamedQuery(name).getSingleResult();
+        return (V) getEntityManager().createNamedQuery(name).getSingleResult();
     }
 
     public <V> V executeSingleNamedQuery(String name, Map<String, Object> params) {
-        Query q = em.createNamedQuery(name);
+        Query q = getEntityManager().createNamedQuery(name);
         for (Map.Entry<String, Object> entry : params.entrySet()) {
             q.setParameter(entry.getKey(), entry.getValue());
         }
@@ -73,13 +73,13 @@ public abstract class CrudPersistence<T> {
     }
 
     public List<T> findByName(String name) {
-        Query q = em.createQuery("select u from " + entityClass.getSimpleName() + " u where u.name like :name");
+        Query q = getEntityManager().createQuery("select u from " + getEntityClass().getSimpleName() + " u where u.name like :name");
         q.setParameter("name", "%" + name + "%");
         return q.getResultList();
     }
 
     public List<T> findByName(String name, Integer page, Integer maxRecords) {
-        Query q = em.createQuery("select u from " + entityClass.getSimpleName() + " u where u.name like :name");
+        Query q = getEntityManager().createQuery("select u from " + getEntityClass().getSimpleName() + " u where u.name like :name");
         q.setParameter("name", "%" + name + "%");
         if (page != null && maxRecords != null) {
             q.setFirstResult((page - 1) * maxRecords);
