f2e8e979c4485db122ca6570ad2352a881aa9b14
Andrés Felipe Esguerra Restrepo <kaosterra@gmail.com>
2015-12-16 23:44:14 -0500

    DateStrategy for PODAM

---
+++ src/main/java/co/edu/uniandes/csw/crud/api/podam/strategy/DateStrategy.java
@@ -0,0 +1,26 @@
+package co.edu.uniandes.csw.crud.api.podam.strategy;
+
+import java.util.Calendar;
+import java.util.Date;
+import java.util.Random;
+import uk.co.jemos.podam.common.AttributeStrategy;
+
+public class DateStrategy implements AttributeStrategy<Date> {
+
+    @Override
+    public Date getValue() {
+        Random r = new Random();
+        Calendar c = Calendar.getInstance();
+        c.set(Calendar.YEAR, r.nextInt(
+                c.getActualMaximum(Calendar.YEAR) - c.getActualMinimum(Calendar.YEAR) + 1)
+                + c.getActualMinimum(Calendar.YEAR));
+        c.set(Calendar.DAY_OF_YEAR, r.nextInt(
+                c.getActualMaximum(Calendar.DAY_OF_YEAR) - c.getActualMinimum(Calendar.DAY_OF_YEAR) + 1)
+                + c.getActualMinimum(Calendar.DAY_OF_YEAR));
+        c.set(Calendar.HOUR_OF_DAY, c.getActualMinimum(Calendar.HOUR_OF_DAY));
+        c.set(Calendar.MINUTE, c.getActualMinimum(Calendar.MINUTE));
+        c.set(Calendar.SECOND, c.getActualMinimum(Calendar.SECOND));
+        c.set(Calendar.MILLISECOND, c.getActualMinimum(Calendar.MILLISECOND));
+        return c.getTime();
+    }
+}
